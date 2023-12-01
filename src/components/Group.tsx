17523d406ccfd50510f2a6599dbba3530657905a
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2023-12-01 13:20:14 +0100

    second commit

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,14 +1,22 @@
 interface groupProps {
   group: string[];
+  index: number;
+  displayGroups: number;
 }
-export default function Group({ group }: groupProps) {
+export default function Group({ group, index, displayGroups }: groupProps) {
+  const showGroups = group.length >= 1;
   return (
-    <div className="group_item">
-      <ul>
-        {group.map((el, i) => (
-          <li key={i}>{el}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      {showGroups && (
+        <div className="group_item">
+          <h2 className="group-class">Groupe {index + 1}</h2>
+          <ul>
+            {group.map((el, i) => (
+              <li key={i}>{el}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,24 +11,26 @@ import Group from "@/components/Group";
 
 export default function Home() {
   const [userInput, setUserInput] = useState("");
-  const [numberOfGroups, setNumberOfGroups] = useState(2);
+  const [numberOfGroups, setNumberOfGroups] = useState(1);
   // const [groups, setGroups] = useState<string[][]>([]);
 
   function shuffle<T>(array: T[]) {
-    let currentIndex = array.length,  randomIndex;
-  
+    let currentIndex = array.length,
+      randomIndex;
+
     // While there remain elements to shuffle.
     while (currentIndex !== 0) {
-  
       // Pick a remaining element.
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-  
+
       // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+        array[randomIndex],
+        array[currentIndex],
+      ];
     }
-  
+
     return array;
   }
 
@@ -39,28 +41,29 @@ export default function Home() {
     .filter((el) => !!el); // ne met pas les chaines de caractères vides
 
   const groups = useMemo(() => {
-    return distributeIntoGroups(studentList, numberOfGroups)
-  }, [studentList, numberOfGroups])
+    return distributeIntoGroups(studentList, numberOfGroups);
+  }, [studentList, numberOfGroups]);
 
   // useEffect(() => {
   //   setGroups(distributeIntoGroups(test, numberOfGroups));
   // }, [test, numberOfGroups]);
 
-  console.log(studentList);
-  console.log(numberOfGroups);
-  console.log("groups", groups)
-
   const handleRandomize = () => {
-    console.log('ramdomize !')
+    console.log("ramdomize !");
     const shuffledList = shuffle([...studentList]);
-    setUserInput(shuffledList.join('\n')); // Mettez à jour l'input avec la liste mélangée
-  }
+    setUserInput(shuffledList.join("\n")); // Mettez à jour l'input avec la liste mélangée
+  };
+
+  const handleClear = () => {
+    setUserInput("");
+    setNumberOfGroups(1);
+  };
 
   return (
     <div className="app">
       <textarea
-        className="text-black border input-field"
-        placeholder="enter students here"
+        className="input-field"
+        placeholder="Enter students here"
         value={userInput}
         name=""
         id=""
@@ -68,25 +71,37 @@ export default function Home() {
           setUserInput(e.target.value);
         }}
       ></textarea>
-        <div className="group_wrapper">
-      <div className="btn-container">
-        <p>Number of groups :</p>
-      <input
-        className="number-group-input"
-        type="number"
-        max={studentList.length}
-        min="1"
-        onChange={(e) => {
-          setNumberOfGroups(parseInt(e.target.value, 10));
-        }}
-        />
-      <button className="randomize-input" onClick={handleRandomize}>
-        Ramdomize
-      </button>
+      <div className="group_wrapper">
+        <div className="btn-container">
+          <p>Number of groups :</p>
+          <input
+            value={numberOfGroups}
+            className="number-group-input"
+            type="number"
+            max={studentList.length}
+            placeholder="2"
+            min="1"
+            onChange={(e) => {
+              setNumberOfGroups(parseInt(e.target.value, 10));
+            }}
+          />
+          <button className="randomize-input" onClick={handleRandomize}>
+            Ramdomize
+          </button>
+          <button className="randomize-input" onClick={handleClear}>
+            Clear
+          </button>
         </div>
         <div className="wrapper">
           {groups.map((group, i) => {
-            return <Group key={i} group={group} />;
+            return (
+              <Group
+                key={i}
+                group={group}
+                index={i}
+                displayGroups={numberOfGroups}
+              />
+            );
           })}
         </div>
       </div>
